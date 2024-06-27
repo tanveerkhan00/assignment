ee4a0e3139654e9d3935855833fd9f8243a9aae5
Tanveer khan <152361941+tanveerkhan00@users.noreply.github.com>
2024-06-27 10:28:38 +0500

    assignment

---
+++ script.js
@@ -0,0 +1,60 @@
+// var marks = prompt("Enter your marks")
+
+
+// if(marks>=90 && marks<=100){
+//         alert("your marks is " + marks + " " + "And your grade is A+"); 
+// }
+// else if (marks>=80 && marks<=89){
+//     alert("your marks is " + marks + " " + "And your grade is A");
+// }
+// else if (marks>=70 && marks<=79){
+//     alert("your marks is " + marks + " " + "And your grade is B");
+// }
+// else if (marks>=60 && marks<=69){
+//     alert("your marks is " + marks  +  " " + "And your grade is C");
+// }
+// else if (marks>=50 && marks<=59){
+//     alert("your marks is " + marks  + " " + "And your grade is D");
+// }
+// else if (marks<=50 && marks>=0){
+//     alert("you have fail");
+// }
+
+
+//////////////////////////////////////////////
+
+// age eligibility
+
+
+// var age;
+// age = +prompt("Enter your age");
+//     if (age >= 18 && age <= 65)
+//         {
+//         alert("Eligible")
+//         }
+//     else
+//     {
+//         alert("Not Eligible")
+//     }
+
+//////////////////////////////////////////////
+
+// Login acess
+
+
+var loginAccess (username, password)
+loginAccess = prompt("Enter your UserName")
+    correct_username = "user123";
+    loginAccess = prompt("Enter your passward")
+    correct_password = "pass123";
+
+    if (username == correct_username || password == correct_password)
+        {
+       alert("Access Granted");
+         }
+    
+    else
+        {
+      alert("Access Denied");
+        }
+    
